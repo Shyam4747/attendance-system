68bf5e2a5d8f20c49658a8721e707ecc67e32839
Shyam4747 <shyam.2353050@srec.ac.in>
2026-06-15 23:23:55 +0530

    Allow Render Mongo password env fallback

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,10 +1,25 @@
 import mongoose from "mongoose";
 
+function getMongoUri(uri) {
+  if (uri?.startsWith("mongodb://") || uri?.startsWith("mongodb+srv://")) {
+    return uri;
+  }
+
+  if (process.env.MONGO_PASSWORD) {
+    const password = encodeURIComponent(process.env.MONGO_PASSWORD);
+    return `mongodb+srv://shyam2353050_db_user:${password}@cluster0.hdzmfgq.mongodb.net/attendance?retryWrites=true&w=majority&appName=Cluster0`;
+  }
+
+  return uri;
+}
+
 export async function connectDatabase(uri) {
-  if (!uri) {
-    throw new Error("MONGODB_URI is required.");
+  const mongoUri = getMongoUri(uri);
+
+  if (!mongoUri) {
+    throw new Error("MONGODB_URI or MONGO_PASSWORD is required.");
   }
 
   mongoose.set("strictQuery", true);
-  await mongoose.connect(uri);
+  await mongoose.connect(mongoUri);
 }
